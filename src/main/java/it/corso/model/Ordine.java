3d9ac5ce840812d8a3de975bf6ac0af5cdb00646
AleAgnelli92 <aleagnelli92@gmail.com>
2024-02-01 17:14:30 +0100

    Update Ordine.java

--- src/main/java/it/corso/model/Ordine.java
+++ src/main/java/it/corso/model/Ordine.java
@@ -5,16 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 public class Ordine {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column
 	private LocalDateTime data;
+	@Column
 	private double importo;
 	
     @ManyToOne(cascade = CascadeType.REFRESH)
