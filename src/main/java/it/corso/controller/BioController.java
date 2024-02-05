f8d125945aeeb3fb2672c5779ede82ff154b27cd
Ionela Miron <im.ionela@icloud.com>
2024-02-05 10:08:41 +0100

    creazione controller bio, fix some bug

--- src/main/java/it/corso/controller/BioController.java
+++ src/main/java/it/corso/controller/BioController.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/")
-public class HomePageController {
-	
+@RequestMapping("/bio")
+public class BioController {
 	@GetMapping
-    public String home() {
-        return "homepage";
+    public String bio() {
+        return "/bio";
     }
 }
