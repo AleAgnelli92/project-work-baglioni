faa578fac12051d75f95f3efd05fc9e237532474
AleAgnelli92 <aleagnelli92@gmail.com>
2024-02-06 18:00:12 +0100

    sistemazione navbar + eliminazione quantità da carrello

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,6 +2,8 @@ const navHome = document.querySelector('.navbar');
 
 var confirmDiv = document.getElementById("confirm");
 
+
+
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
         navHome.classList.add('navbar-scrolled')
@@ -15,7 +17,9 @@ document.querySelector('.navbar')[
 ]('click', function() {
     this.style.backgroundColor = this.style.backgroundColor === 'rgba(0, 0, 0, 0.85)' ?
         'transparent' : 'rgba(0, 0, 0, 0.85)';
+ 
 });
+
 
 function mostraAlert() {
     var customAlert = document.getElementById('customAlert');
@@ -56,6 +60,9 @@ function confermaEliminazione(albumId) {
          confirmDiv.style.display = "none";
       }
     
+    
+ 
+
 
 
 
