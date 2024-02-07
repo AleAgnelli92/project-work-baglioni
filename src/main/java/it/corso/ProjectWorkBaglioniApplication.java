60842d5fb5b3aae50b13cb589eb6f3ae35d5926f
teslacx <tesla.d.a@hotmail.com>
2024-02-07 10:34:42 +0100

    test Tesla

--- src/main/java/it/corso/ProjectWorkBaglioniApplication.java
+++ src/main/java/it/corso/ProjectWorkBaglioniApplication.java
@@ -8,6 +8,7 @@ public class ProjectWorkBaglioniApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWorkBaglioniApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
