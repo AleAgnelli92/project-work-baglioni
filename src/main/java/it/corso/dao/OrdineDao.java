c33e898a68d435428404edc1718c31c3345d0462
teslacx <tesla.d.a@hotmail.com>
2024-02-07 11:09:18 +0100

    ignored ds

--- src/main/java/it/corso/dao/OrdineDao.java
+++ src/main/java/it/corso/dao/OrdineDao.java
@@ -1,10 +1,10 @@
 package it.corso.dao;
 
-import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.CrudRepository;
 import it.corso.model.Ordine;
 
 public interface OrdineDao extends CrudRepository<Ordine, Integer>
 {
-
+	
 }
