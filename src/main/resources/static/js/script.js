b98a6dcfe8009867cf95b265345f1de81e18e35a
AleAgnelli92 <aleagnelli92@gmail.com>
2024-02-08 16:37:26 +0100

    Modifiche grafiche homepage

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,9 @@
 const navbar = document.querySelector('.navbar');
 var confirmDiv = document.getElementById("confirm");
-let mediaQuery = window.matchMedia('(min-width: 992px)')
+//let mediaQuery = window.matchMedia('(min-width: 992px)')
 
-function noScrollMobile(mediaQuery) {
-	if (mediaQuery.matches) {
+//function noScrollMobile(mediaQuery) {
+	//if (mediaQuery.matches) {
 		window.addEventListener('scroll', () => {
 		    if (window.scrollY >= 56) {
 		        navbar.classList.add('navbar-scrolled');
@@ -11,18 +11,17 @@ function noScrollMobile(mediaQuery) {
 		        navbar.classList.remove('navbar-scrolled');
 		    }
 		});
-	}
-}
-
-mediaQuery.addEventListener('change', noScrollMobile);
-
-noScrollMobile(mediaQuery);
+	//}
+//}
+//mediaQuery.addEventListener('change', noScrollMobile);
+//noScrollMobile(mediaQuery);
 
 document.querySelector('.navbar-toggler')[
     document.addEventListener ? 'addEventListener' : 'attachEvent'
 ]('click', function() {
-    navbar.style.backgroundColor = navbar.style.backgroundColor === 'rgba(0, 0, 0, 0.85)' ?
-        'transparent' : 'rgba(0, 0, 0, 0.85)';
+	
+    navbar.style.backgroundColor = navbar.style.backgroundColor === 'RGBA(10,42,54,0.65)' ?
+        'transparent' : 'RGBA(10,42,54,0.65)';
         navbar.style
 });
 
@@ -65,22 +64,18 @@ function chiudiAlertAccesso() {
 function chiudiAlertOrdini() {
     var customAlert = document.getElementById('customAlert');
     customAlert.style.display = 'none';
-
-  
 }
 
 function confermaEliminazione(albumId) {
         window.location.href = "/carrello/rimuovi?id=" + albumId;
-        }
+}
     
-    
-
 function showConfirm() {
          confirmDiv.style.display = "block";
-      }
+}
 function closeConfirm() {
          confirmDiv.style.display = "none";
-      }
+}
  
 
 
