14a827c2070afd216c423fe3aa3a8fd0268c02f6
Irene Macchi <irenemacchi01@gmail.com>
2024-02-09 12:34:44 +0100

    Update Login e Registrazione html e controller
    Separato form registrazione da form login - c'è da vedere e correggere una cosa nel pomeriggio

--- src/main/java/it/corso/controller/LoginController.java
+++ src/main/java/it/corso/controller/LoginController.java
@@ -3,17 +3,14 @@ package it.corso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.corso.model.Cliente;
 import it.corso.service.ClienteService;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 // localhost:8080/login
@@ -35,6 +32,7 @@ public class LoginController {
         return "login";
     }
 	
+	
 	@PostMapping("/controllo")
 	public String loginManager(@RequestParam("username") String username,
 							  @RequestParam("password") String password,
@@ -43,17 +41,6 @@ public class LoginController {
 		if(!clienteService.controlloLogin(username, password, session))
 			return "redirect:/login";
 		
-		return "redirect:/personale";
-	}
-	
-	
-	@PostMapping("/registrazione")
-	public String formManager(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result) {
-		
-		if(result.hasErrors())
-			return "/login";
-		
-		clienteService.registrazioneCliente(cliente);
 		return "redirect:/personale";
 	}
 	
--- src/main/java/it/corso/controller/RegistrazioneController.java
+++ src/main/java/it/corso/controller/RegistrazioneController.java
@@ -1,16 +1,41 @@
 package it.corso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.corso.model.Cliente;
+import it.corso.service.ClienteService;
+import jakarta.validation.Valid;
 
 @RequestMapping("/registrazione")
 @Controller
 public class RegistrazioneController
 {
-    @GetMapping
-    public String getPage()
-    {
-        return "registrazione";
-    }
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	
+	@GetMapping
+	public String getPage(Model model) {
+	    model.addAttribute("cliente", new Cliente());
+	    return "registrazione"; 
+	}
+
+	
+	@PostMapping("/registrazionecliente")
+	public String formManager(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result) {
+		
+		if(result.hasErrors())
+			return "/registrazione";
+		
+		clienteService.registrazioneCliente(cliente);
+		return "redirect:/personale";
+	}
 }
